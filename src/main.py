579ca60c33545ca15da11a5ecfaa156fb0e81b3f
Stanislaw <62724833+BoberITman@users.noreply.github.com>
2023-05-15 23:16:16 +0200

    Blueprints

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+import os
+from typing import Union
+
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.exceptions import RequestValidationError
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse, StreamingResponse
 from starlette.status import HTTP_401_UNAUTHORIZED
-from typing import Union
-from db import db_session
-import os
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from db import db_session
+
+app = FastAPI(
+    title="GractwoApi",
+    version="0.1.0",
+)
+
+app.include_router()
+app.include_router()
